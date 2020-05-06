5f03e4526dbe1723457f881f5971cc4b2871c99e
liuboxun <liuboxun@gozap.com>
2020-05-06 19:16:51 +0800

    :sparkles: feat(1.0): add fish tank web controller
    Signed-off-by: liuboxun <liuboxun@gozap.com>

---
+++ src/test/java/cn/linuxcrypt/fish/tank/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.linuxcrypt.fish.tank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
